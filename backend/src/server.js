d85809c73d6aff2f4fb447a85b6a7348ca1399f5
rohanvinkare <rohanvinkare2022@gmail.com>
2025-11-04 23:58:25 +0530

    home route

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,6 +25,11 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
